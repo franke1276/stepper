f0fed5e82d505f2d348629deb09cad7175d769f4
Christian Franke <chriss@frankeonline.net>
2018-09-18 10:55:36 +0200

    beschleunigung nach stepps

--- stepper.ino
+++ stepper.ino
@@ -27,40 +27,94 @@ double speed = 60;
 
 
 struct motor {
-  int stepPin;
-  int dirPin;
   int state;
   unsigned long steps;
   unsigned long t0;
-  double speed;
-  bool forward;
+  double desiredSpeed;
+  double currentSpeed;
+  bool forwardLeft;
+  bool forwardRight;
 };
 
 typedef struct motor Motor;
 
-Motor motorLeft = {MOTOR_LEFT_STEP, MOTOR_LEFT_DIR, 0, 0, micros(), 0, true};
-Motor motorRight = {MOTOR_RIGHT_STEP, MOTOR_LEFT_DIR, 0, 0, micros(), 0, true};
+Motor m = {0, 0, micros(), 0, 0, true, true};
+
 
 unsigned long t1 = micros();
+const double a0 = 50;
 
 void motorSpeed(unsigned long now, struct motor& m) {
-  if (m.speed == 0) {
+  static unsigned long ta = now;
+  static unsigned long tb = now;
+  static double a = 0;
+  static int accelerationState = 0;
+  int steps = 100;
+  static double deltaVPerStep = 0;
+  static int count = 0;
+  int maxCount = 0;
+
+
+  switch(accelerationState) {
+    case 0: // hold speed phase
+      if (m.currentSpeed != m.desiredSpeed) {
+        accelerationState = 1;
+        deltaVPerStep = (m.desiredSpeed - m.currentSpeed) / steps;
+        m.currentSpeed += deltaVPerStep;
+        count = 1;
+        tb = now;
+      }
+      break;
+    case 1: // acceleration phase
+      if (now > (tb + 1000000 / steps)) {
+        m.currentSpeed += deltaVPerStep;
+        
+        if (count == steps) {
+          m.currentSpeed = m.desiredSpeed;
+          accelerationState = 0;
+        } else {
+          count++;  
+          tb = now;
+        }
+        Serial.print("1: ");
+        Serial.println(m.currentSpeed);
+      }
+      break;
+  }
+  
+//  if (now > (ta + 10000)) {
+//    double t = (now - ta) / (1000.0 * 1000.0 );
+//    
+//    if (m.currentSpeed > m.desiredSpeed) {
+//      a = -a0;
+//    } else if (m.currentSpeed < m.desiredSpeed) {
+//      a = a0;
+//    } else {
+//      a = 0;
+//    }
+//    m.currentSpeed += a * t;
+//    ta = now;  
+//  }
+  
+  if (m.currentSpeed == 0) {
     return;
   }
-  unsigned long untilTime = (1.0 / (m.speed/60.0 * stepsPerCycle * 4)) * 1000 * 1000;
+  unsigned long untilTime = (1.0 / (m.currentSpeed/60.0 * stepsPerCycle * 4)) * 1000 * 1000;
+  digitalWrite(MOTOR_LEFT_DIR, m.forwardLeft? HIGH : LOW);
+  digitalWrite(MOTOR_RIGHT_DIR, m.forwardRight? LOW: HIGH);
   switch(m.state) {
     case 0:
       if (now > t0 + untilTime) {
-        digitalWrite(m.dirPin, m.forward? HIGH : LOW);
-        digitalWrite(m.stepPin, HIGH); 
+        digitalWrite(MOTOR_LEFT_STEP, HIGH); 
+        digitalWrite(MOTOR_RIGHT_STEP, HIGH); 
         m.t0 = micros();  
         m.state = 1;
       }
       break;
      case 1:
       if (now > m.t0 + untilTime) {
-        digitalWrite(m.dirPin, m.forward? HIGH : LOW);
-        digitalWrite(m.stepPin, LOW); 
+        digitalWrite(MOTOR_LEFT_STEP, LOW); 
+        digitalWrite(MOTOR_RIGHT_STEP, LOW); 
         m.t0 = micros();  
         m.state = 0;
         m.steps++;
@@ -68,33 +122,25 @@ void motorSpeed(unsigned long now, struct motor& m) {
       break; 
   }
 }
-double a = 40; // cyclesPerMinute^2
-unsigned long ta = micros();
+
 
 double t = 0;
 void loop() {
   unsigned long now = micros();
-  motorSpeed(now, motorLeft);
-  motorSpeed(now, motorRight);
-
-  if (now > (ta + 10000)) {
-    t = (now - ta) / (1000.0 * 1000.0 );
-    
-    motorLeft.speed += a * t;
-    motorRight.speed += a * t;
-    if (motorLeft.speed >= 60) {
-      a *= -1;
-    } 
-    if (motorLeft.speed <= 0) {
-      a *= -1;
-    }
-    ta = now;  
-  }
+  motorSpeed(now, m);
   
 
-  if (now > t1 + 1000000) {
-    Serial.println(motorLeft.speed);
+  
+
+  if ((now > t1 + 3000000)  && m.desiredSpeed == 0 ) {
+    m.desiredSpeed=30;
     t1 = now;
   }
+  
+  if ((now > t1 + 3000000) && m.desiredSpeed == 30) {
+    m.desiredSpeed=0;
+    t1 = now;
+  }
+  
  
 }
