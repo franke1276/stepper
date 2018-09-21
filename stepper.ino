cc83e55f28c7346cd9e2a44c5f19eeee9e5ec560
Christian Franke <chriss@frankeonline.net>
2018-09-21 23:48:02 +0200

    distance with interrupts, bit better

--- stepper.ino
+++ stepper.ino
@@ -12,9 +12,13 @@ const int enPin = 8;
 
 const int stepsPerCycle = 200 * 8; 
 const double d_r = 0.067;
-const double d_k = 0.168 + 0.027/2; // try and error
+const double d_k = 0.168 + 0.027/2 + 0.005; // try and error
+//const double d_k = 0.168 + 0.031; // try and error
+//const double d_k = 0.168 ; 
 const double u_fullturn = d_k / d_r;
 const double u_halfturn = u_fullturn / 2.0;
+
+const double u_one_cm = 1.0 / (d_r * PI * 100.0 );
 
 void setup() {
   pinMode(enPin,OUTPUT);
@@ -115,42 +119,100 @@ bool accelerationCompleted() {
   return m.desiredSpeed == m.currentSpeed;
 }
 
-unsigned long calculateStepsForAcceleration(double desiredSpeed) {
-  double deltaV = desiredSpeed -m.currentSpeed;
+long calculateStepsForAcceleration(double currentSpeed, double desiredSpeed) {
+  double deltaV = desiredSpeed - currentSpeed;
   double t = deltaV / m.acceleration;
-  return  1.0 / 2.0 * m.acceleration / 60.0 * stepsPerCycle * t * t + m.currentSpeed / 60.0 * stepsPerCycle * t;
+  double result =  1.0 / 2.0 * m.acceleration / 60.0 * stepsPerCycle * t * t + currentSpeed / 60.0 * stepsPerCycle * t;
+  return  abs(result);
 }
 
 
 volatile unsigned long echo_start = 0;
-volatile double distance = -1;
-volatile int exCounterH = 0;
-volatile int exCounterL = 0;
+volatile double distance = 10000;
 void entfernung() {
-  
+  noInterrupts();
 
   switch (digitalRead(2)){
     case HIGH:                                      
-      exCounterH++;
       echo_start = micros();                        
       break;
       
     case LOW:      
-      exCounterL++;                                 
       distance = (micros() - echo_start)  * 100.0 * 340.0/(2.0 * 1000.0 * 1000.0);
       detachInterrupt(digitalPinToInterrupt(2));
       break;
   }
+  interrupts();
 }
 
 
 void loop() {
-  static unsigned long stepsStart = 0;
+  static long stepsStart = 0;
   static unsigned long t_main = micros();
   static int mainState = 0;
 
   unsigned long now = micros();
   motorSpeed(now, &m);
+
+
+//  switch (mainState) {
+//    case 0: // wait for start
+//      if ((now > t_main + 3 * 1000000)) {
+//        mainState = 1;
+//      }
+//      break;
+//
+//    case 1: // start
+//      stepsStart = m.steps;
+//      m.desiredSpeed=20;
+//      m.forwardLeft = true;
+//      m.forwardRight = false;
+//      mainState = 2;
+//      break;  
+//    case 2: 
+////          Serial.print(u_one_cm);
+////      Serial.print(" ");
+////      Serial.print(stepsPerCycle * u_one_cm * 15);
+////      Serial.print(" ");
+////      Serial.print(calculateStepsForAcceleration(m.desiredSpeed, 0));
+////      Serial.print(" ");
+////      Serial.print(stepsStart);
+////      Serial.print(" ");
+////      
+////      Serial.println((stepsStart - calculateStepsForAcceleration(m.desiredSpeed, 0) + stepsPerCycle * u_one_cm * 15));
+//
+//      
+//
+////      Serial.println(stepsStart - calculateStepsForAcceleration(0) + stepsPerCycle * u_one_cm * 15);
+//      if (m.steps >= (stepsStart - calculateStepsForAcceleration(m.desiredSpeed, 0) + stepsPerCycle * u_halfturn)) {       
+//        m.desiredSpeed=0;
+//        mainState = 3;
+//        stepsStart = m.steps;
+//        Serial.println(calculateStepsForAcceleration(20, 0));
+//        
+//      }  
+//      break;
+//    case 3:
+//        if (accelerationCompleted()) {
+//          mainState = 4;
+//          Serial.println(m.steps - stepsStart);
+//        }
+//      break;
+//    case 4:
+//      break;
+//  }
+//  
+
+
+
+
+
+
+
+
+
+
+
   
   switch (mainState) {
     case 0: // wait for start
@@ -168,14 +230,13 @@ void loop() {
     case 2: // wait for acceleration
       if (accelerationCompleted()) {
         mainState = 3;
+        distance = 10000;
         t_main = now;
       }
       break;
     case 3:  // strait forward
-      if (now > (t_main + 100 * 1000)) {
-//        Serial.println(distance);
-        
-        
+      if (now > (t_main + 200 * 1000)) {
+        Serial.println(distance);
         digitalWrite(trigger, LOW);
         delayMicroseconds(3);
         noInterrupts();
@@ -187,35 +248,48 @@ void loop() {
         t_main = now;
       }
       
-      if (distance < 25) {
+      if (distance < 10) {
         m.desiredSpeed=0;
         mainState = 4;
       }
-//      if (now > (t_main + 3 * 1000000)) {
-//        m.desiredSpeed=0;
-//        mainState = 4;
-//      }
       break;
     case 4: // wait for break
       if (accelerationCompleted()) {
         mainState = 5;
         stepsStart = m.steps;
         m.forwardLeft = false;
+        m.forwardRight = false;
+        m.desiredSpeed = 20;
+        stepsStart = m.steps;
+      }
+      break;  
+    case 5:  // wait for 15 cm back
+//      Serial.print(u_one_cm);
+//      Serial.print(" ");
+//      Serial.print(stepsPerCycle * u_one_cm * 15);
+//      Serial.print(" ");
+//      Serial.println(calculateStepsForAcceleration(0));
+      if (m.steps >= (stepsStart - calculateStepsForAcceleration(m.desiredSpeed, 0) + stepsPerCycle * u_one_cm * 20)) {       
+        m.desiredSpeed=0;
+        mainState = 6;
+      }  
+      break;
+    case 6: // wait for break
+      if (accelerationCompleted()) {        
+        mainState = 7;
+        stepsStart = m.steps;
+        m.forwardLeft = false;
+        m.forwardRight = true;
         m.desiredSpeed = 20;
       }
       break;  
-    case 5: // wait for acceleration
-      if (accelerationCompleted()) {        
-        mainState = 6;
-      }
-      break;  
-    case 6:  // wait for turn completed
-      if (m.steps >= (stepsStart - calculateStepsForAcceleration(0) + stepsPerCycle * u_halfturn)) {       
+    case 7:  // wait for turn completed
+      if (m.steps >= (stepsStart - calculateStepsForAcceleration(m.desiredSpeed, 0) + stepsPerCycle * u_halfturn)) {       
         m.desiredSpeed=0;
-        mainState = 7;
+        mainState = 8;
       }  
       break;
-    case 7: // wait for break
+    case 8: // wait for break
       if (accelerationCompleted()) {        
         mainState = 1;
       }
